2c26df88ac161a2f34d11bed9d05e132c296c243
testirovHTML <116376184+testirovHTML@users.noreply.github.com>
2022-10-22 21:14:36 +0400

    Add files via upload

---
+++ Send.php
@@ -0,0 +1,22 @@
+<?php
+if( !empty($_POST) ) {
+        $message = 
+        "Новое сообщение от пользователя " . $_POST['username'] . 
+        "\n" ."Отправитель: " . $_POST['username'] . 
+        "\n" . "E-mail: " . $_POST['usermail'] . 
+        "\n" . "Сообщение: 
+        \n " . $_POST['message'];
+
+        $sendMail = mail( "marina.perevozchikova.26@mail.ru", "Новое сообщение", $message );
+        if( $sendMail ) {
+            echo "Письмо успешно отправлено!";
+			echo "<br /><br /><a href='/'>Вернуться на сайт.</a>";
+        } else {
+            echo "Упс ... кажется что-то пошло не так";
+        }
+    }
+?>
+<script language="JavaScript" type="text/javascript">
+function changeurl(){eval(self.location="/");}
+window.setTimeout("changeurl();",3000);
+</script>
